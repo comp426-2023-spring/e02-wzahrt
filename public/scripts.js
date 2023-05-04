af8c0644464d2bdc9d10be4082de4474a7c40972
wzahrt <wzahrt@unc.edu>
2023-05-04 18:05:48 -0400

    created most of the frontend except for the info button

--- public/scripts.js
+++ public/scripts.js
@@ -2,9 +2,69 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-  var show = 0;
+  var show1 = 0;
+  var show2 = 0;
+  var rps = false;
+  var rpsls = false;
 
-  function toggle() {
-    show += 1;
-    if 
+  function toggleOG() {
+    var againstOpponent = document.getElementsByClassName("againstOpponent")[0];
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var spock = document.getElementsByClassName("rpsls")[1];
+    var ogRPS = document.getElementsByClassName("ogRPS")[0];
+    show1 += 1;
+
+    if (show1 % 2 == 1) {
+        if (show2 % 2 == 1) {
+            ogRPS.style.display = 'inline';
+        }
+        againstOpponent.style.display = 'inline';
+        lizard.style.display = 'none';
+        spock.style.display = 'none';
+        rps = true;
+    }
+    else {
+        againstOpponent.style.display = 'none';
+        ogRPS.style.display = 'none';
+        rps = false;
+    }
+  }
+
+  function toggleNew() {
+    var againstOpponent = document.getElementsByClassName("againstOpponent")[0];
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var spock = document.getElementsByClassName("rpsls")[1];
+    var ogRPS = document.getElementsByClassName("ogRPS")[0];
+
+    show1 += 1;
+    
+
+    if (show1 % 2 == 1) {
+         if (show2 % 2 == 1) {
+            ogRPS.style.display = 'inline';
+        }
+        againstOpponent.style.display = 'inline';
+        lizard.style.display = 'inline';
+        spock.style.display = 'inline';
+        rpsls = true;
+    }
+    else {
+        againstOpponent.style.display = 'none';
+        lizard.style.display = 'none';
+        spock.style.display = 'none';
+        ogRPS.style.display = 'none';
+        rpsls = false;
+    }
+  }
+
+  function toggle_against_opponent() {
+    var ogRPS = document.getElementsByClassName("ogRPS")[0];
+    show2 += 1;
+    
+    if ((show1 % 2 == 1) && (show2 % 2 == 1)) {
+        ogRPS.style.display = 'inline';
+    }
+    else {
+         ogRPS.style.display = 'none';
+    }
   }
