407890172d0c2a353554787b20d5fdf63059c8ef
wzahrt <wzahrt@unc.edu>
2023-05-07 18:17:51 -0400

    finished the project! added final photos to docs

--- public/scripts.js
+++ public/scripts.js
@@ -6,26 +6,29 @@
   var show2 = 0;
   var rps = false;
   var rpsls = false;
+  var againstOpponent = false;
 
   function toggleOG() {
     var againstOpponent = document.getElementsByClassName("againstOpponent")[0];
     var lizard = document.getElementsByClassName("rpsls")[0];
     var spock = document.getElementsByClassName("rpsls")[1];
-    var ogRPS = document.getElementsByClassName("ogRPS")[0];
+    var play = document.getElementsByClassName("play")[0];
     show1 += 1;
 
     if (show1 % 2 == 1) {
         if (show2 % 2 == 1) {
-            ogRPS.style.display = 'inline';
+            play.style.display = 'inline';
         }
         againstOpponent.style.display = 'inline';
         lizard.style.display = 'none';
         spock.style.display = 'none';
-        rps = true;
+        play.style.display = 'inline';
+        rpsls = false;
+        rps = true
     }
     else {
         againstOpponent.style.display = 'none';
-        ogRPS.style.display = 'none';
+        play.style.display = 'none';
         rps = false;
     }
   }
@@ -34,37 +37,100 @@
     var againstOpponent = document.getElementsByClassName("againstOpponent")[0];
     var lizard = document.getElementsByClassName("rpsls")[0];
     var spock = document.getElementsByClassName("rpsls")[1];
-    var ogRPS = document.getElementsByClassName("ogRPS")[0];
+    var play = document.getElementsByClassName("play")[0];
 
     show1 += 1;
     
 
     if (show1 % 2 == 1) {
          if (show2 % 2 == 1) {
-            ogRPS.style.display = 'inline';
+            play.style.display = 'inline';
         }
         againstOpponent.style.display = 'inline';
         lizard.style.display = 'inline';
         spock.style.display = 'inline';
+        play.style.display = 'inline';
+        rps = false;
         rpsls = true;
     }
     else {
         againstOpponent.style.display = 'none';
         lizard.style.display = 'none';
         spock.style.display = 'none';
-        ogRPS.style.display = 'none';
+        play.style.display = 'none';
         rpsls = false;
     }
   }
 
   function toggle_against_opponent() {
-    var ogRPS = document.getElementsByClassName("ogRPS")[0];
     show2 += 1;
     
     if ((show1 % 2 == 1) && (show2 % 2 == 1)) {
-        ogRPS.style.display = 'inline';
+      againstOpponent = true;
     }
     else {
-         ogRPS.style.display = 'none';
+      againstOpponent = false;
     }
+  }
+
+  function openRules() {
+    var rules = document.getElementsByClassName("rules")[0];
+  
+    rules.style.display = 'inline';
+  }
+  
+  function closeRules() {
+    var rules = document.getElementsByClassName("rules")[0];
+  
+    rules.style.display = 'none';
+  }
+
+  function reset() {
+    show1 = 0;
+    show2 = 0;
+    rps = false;
+    rpsls = false;
+    againstOpponent = false;
+    location.reload();
+  }
+
+  async function play() {
+    var i = document.getElementById("shot");
+    var shot = i.value;  
+  
+    var player = document.getElementById("player");
+    var opponent = document.getElementById("opponent");
+    var result = document.getElementById("result");
+    var pop_up = document.getElementsByClassName("results-pop-up")[0];
+    
+    if (rps) {
+      if (shot && againstOpponent) {
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const data = await response.json();
+  
+        player.innerHTML = `Player: ${data.player}.`
+        opponent.innerHTML = `Opponent: ${data.opponent}.`
+        result.innerHTML = `Result: ${data.result}.`
+        pop_up.style.display = 'inline'
+        console.log(data);
+      } else {
+        const response = await fetch(`/app/rps/play`);
+        const data = await response.json();
+        console.log(data);
+      }
+    } else {
+      if (shot && againstOpponent) {
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const data = await response.json();
+        player.innerHTML = `Player: ${data.player}.`
+        opponent.innerHTML = `Opponent: ${data.opponent}.`
+        result.innerHTML = `Result: ${data.result}.`
+        pop_up.style.display = 'inline'
+        console.log(data);
+      } else {
+        const response = await fetch(`/app/rpsls/play`);
+        const data = await response.json();
+        console.log(data);
+      }
+    }    
   }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 import { createRequire } from 'node:module';
+import { rps, rpsls } from './lib/rpsls.js'
 const require = createRequire(import.meta.url);
 // The above two lines allow us to use ES methods and CJS methods for loading
 // dependencies.
@@ -71,6 +72,42 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
+app.get('/app/rps', (req, res) => {
+	res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+	res.status(200).send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.query.shot));
+})
+
+app.get('/app/rps/play/:arg', (req, res) => {
+	res.status(200).send(rps(req.params.arg));
+})
+
+app.get('/app/rpsls/play/:arg', (req, res) => {
+	res.status(200).send(rpsls(req.params.arg));
+})
+
+app.get('*', (req, res) => {
+	res.status(404).send('404 NOT FOUND');
+})
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
